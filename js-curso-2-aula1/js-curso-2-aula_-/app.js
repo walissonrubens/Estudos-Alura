98668f6a6923b1be08a60a7b58ffd3dd3eb70e9f
walissonrubens <rubens.08p@gmail.com>
2024-02-10 11:55:43 -0300

    criei desafio 1

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1')
+let paragrafo = document.querySelector('p')
+
+titulo.innerHTML = 'Jogo do número secreto'
+paragrafo.innerHTML = 'escolha um numero entre 1 e 10'
+
+function verificarChute(){
+    console.log('Clicou')
+
+}
---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,11 @@
+let titulo = document.querySelector('h1')
+titulo.innerHTML = 'Hora do desafio'
+
+function verificarConsole(){
+    console.log('o botão foi clicado')
+
+}
+
+function verificarAlert(){
+    alert('Eu amo JS')
+}
